af4475bd9df560f2e0b70e3e1c8b86db32e1e5bf
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-11-23 16:06:00 +0700

    :wrench: Remove draggable marker

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -41,7 +41,6 @@
             v-for="(m, index) in markers"
             :position="m.position"
             :clickable="true"
-            :draggable="true"
             @click="center=m.position"
           ></gmap-marker>
         </gmap-map>
