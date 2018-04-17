e67293aa5d44d42f73123b98d0348976072bf18e
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-04-17 14:22:25 +0700

    :card_file_box: Change to mocked data

--- src/libraries/constants.js
+++ src/libraries/constants.js
@@ -2,4 +2,8 @@ export const BASE_URL = process.env.NODE_ENV === 'production'
   ? 'https://kykungz.github.io/ywc15-announcement/#/'
   : 'localhost:8080/#/'
 
-export const API_URL = 'https://ywc15.ywc.in.th/api/interview'
+/*
+  OLD API_URL
+  export const API_URL = 'https://ywc15.ywc.in.th/api/interview'
+ */
+export const API_URL = 'https://raw.githubusercontent.com/kykungz/ywc15-announcement/master/package.json'
